2ef4a83412ad5b17c3e971f5bd56224a61bc6b0b
joolie <juhee87@gmail.com>
2023-10-20 19:14:00 -0400

    add delete button

--- src/app/cours-details/cours-details.component.ts
+++ src/app/cours-details/cours-details.component.ts
@@ -11,4 +11,37 @@ export class CoursDetailsComponent {
   @Input() courses: any[] = [];
   @Input() currentCourseID: number = 0;
   @Input() currentCourseSemester: string = '';
+
+  // editingStudent: any = null;
+
+  // editGrade(student: any) {
+  //   this.editingStudent = student;
+  // }
+
+  // saveGrade() {
+  //   if (this.editingStudent) {
+  //     this.editingStudent = null;
+  //   }
+
+
+  // Method to confirm and remove a student
+  confirmRemove(student: any) {
+    const confirmation = window.confirm("Êtes-vous sûr ?"); // Display a confirmation dialog
+
+    if (confirmation) {
+      // User confirmed, proceed with removal
+      this.removeStudent(student);
+    }
+  }
+
+  // Method to remove a student from the list
+  removeStudent(student: any) {
+    // Implement the removal logic here, e.g., remove the student from the 'students' array.
+    const studentIndex = this.students.indexOf(student);
+
+    if (studentIndex !== -1) {
+      this.students.splice(studentIndex, 1);
+    }
+  }
 }
+
